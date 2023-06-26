3c21ec55a00cb3446acdd774175569e8c28b6139
lhjwork <jin.come.up.business@gmail.com>
2023-06-26 17:32:44 +0900

    magagize_month

--- magazine_month.php
+++ magazine_month.php
@@ -1,19 +1,20 @@
 <? 
 
-include './core/config.php';
+include $_SERVER['DOCUMENT_ROOT']."/lib_inc.php";
 
-// 인스턴스 생성
-$notice = new Notice($DB);
 
-$list = $notice -> get_all();
+// 메인페이지 -> 이달의 메거진 대표로 보여주고자하는 매거진 정보 전달
+try {
+    
+    $sql = "select * from medical_t where mdt_main = 'Y' and mdt_show = 'Y' order by idx desc limit 1";
+    $mrow = $DB->select_assoc($sql);
 
-// if($list){
-//     foreach($list as $row){
+    echo json_encode($mrow);
 
-//     }
+}catch (Exception $e){
+    header("HTTP/1.0 404 Not Found");
+    echo json_encode(array('error' => $e->getMessage()));
+}
 
-// }
-
-echo json_encode($list);
 
 ?>
--- read_all.php
+++ read_all.php
@@ -4,7 +4,7 @@ include $_SERVER['DOCUMENT_ROOT']."/lib_inc.php";
 
 
 $sql = "SELECT * FROM notice_t";
-$row = $DB -> select_query($sql);
+$row = $DB -> select_assoc($sql);
 
 echo json_encode($row);
 
